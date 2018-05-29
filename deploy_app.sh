3ee8f86370c2bf638e475d0892b74f764cb13bb2
radudd <39598837+radudd@users.noreply.github.com>
2018-05-30 01:17:50 +0200

    Updated APP_PORT => TF_VAR_app_port

--- deploy_app.sh
+++ deploy_app.sh
@@ -7,7 +7,7 @@ if [[ -n $DOKKU_HOST ]]; then
     git remote add dokku dokku@${DOKKU_HOST}:${APP_NAME}
     git fetch --unshallow
     until ssh -q -o StrictHostKeyChecking=no ${DOKKU_HOST} -l dokku ssh-keys:list; do echo "[WARN] Dokku not ready ... please wait" && sleep 30; done; git push dokku master
-    echo http://${DOKKU_HOST}:${APP_PORT}
+    echo http://${DOKKU_HOST}:${TF_VAR_app_port}
   else echo "[ERROR] Dokku host not defined..."
     exit 99
 fi
